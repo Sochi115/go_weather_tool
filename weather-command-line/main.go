e3a295c3ee6ce92591f183f87a6fd88f452c2848
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2023-08-01 22:39:08 +0700

    Added simple GET endpoint

--- weather-command-line/main.go
+++ weather-command-line/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func helloWorld(c *gin.Context) {
+	fmt.Fprintf(c.Writer, "Hello World")
+}
+func handleRequests() {
+	router := gin.Default()
+
+	router.GET("/", helloWorld)
+
+	http.ListenAndServe(":8082", router)
+}
+
 
 func main() {
-	fmt.Printf("Hello World") 
+
+	handleRequests()
 }

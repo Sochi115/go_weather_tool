2d6f1ed7650ec4f81f0f969ceeb21ac1e292a8a2
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2023-08-05 23:17:24 +0700

    Created weather data struct

--- weather-command-line/main.go
+++ weather-command-line/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-
 	// "github.com/gin-gonic/gin"
 )
 
@@ -15,29 +15,59 @@ var CLIENT *http.Client = &http.Client{}
 // 	fmt.Fprintf(c.Writer, "Hello World")
 // }
 
-func getGeoCode(country string) string{
-	api := fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%v&limit=5&apikey=%v", country, API_KEY )
+func getGeoCode(country string) (float64, float64) {
+	api := fmt.Sprintf("http://api.openweathermap.org/geo/1.0/direct?q=%v&limit=5&apikey=%v", country, API_KEY)
 
-	resp , err := CLIENT.Get(api)
-	if err!= nil {
+	resp, err := CLIENT.Get(api)
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
-	if err!= nil {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var x interface{}
+
+	json.Unmarshal(body, &x)
+
+	locations := x.([]interface{})
+
+	area := locations[0]
+	areaMap := area.(map[string]interface{})
+
+	lat := areaMap["lat"]
+	lon := areaMap["lon"]
+	return lat.(float64), lon.(float64)
+}
+
+func getWeatherForecast(lat float64, long float64) WeatherData {
+	api := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%v&lon=%v&appid=%v",lat, long, API_KEY)
+
+	resp, err := CLIENT.Get(api)
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	return string(body)
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	x := WeatherData{}
+	json.Unmarshal(body, &x)
+	return x
 
 }
-// func getWeatherForecast(lat float64, long float64){
-// 	api := fmt.Sprintf("api.openweathermap.org/data/2.5/forecast/daily?lat=%v&lon=%v&cnt=3&appid=%v",lat, long, API_KEY)
-
-// }
 
 func main() {
-	resp := getGeoCode("Phnom Penh")
-	fmt.Println(resp)
+	// lat, lon := getGeoCode("Phnom Penh")
+	lat := 11.5682711
+	lon :=  104.9224426
+	x := getWeatherForecast(lat, lon)
+
+	fmt.Println(x)
 }

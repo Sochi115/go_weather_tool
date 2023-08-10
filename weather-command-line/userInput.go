0c0bc851b68431baaaab6d840bfdc2e101dfb7cb
Chiv Sochivoath <chiv.sochivoath@gmail.com>
2023-08-10 22:12:00 +0700

    Added functionality to choose between multiple locations

--- weather-command-line/userInput.go
+++ weather-command-line/userInput.go
@@ -10,3 +10,32 @@ func getUserCity() string {
 
 	return city
 }
+
+func getLocationIndex(locations []interface{}) int16{
+	fmt.Println("Multiple locations detected, select the correct location:")
+
+	length := len(locations)
+	for i, v := range locations {
+		area := v.(map[string]interface{})
+
+		state := area["state"]
+		country := area["country"]
+		fmt.Printf("%v. %v %v \n", i+1, state, country)
+	}
+
+	return getUserIndexChoice(length)
+}
+
+func getUserIndexChoice(length int) int16 {
+	var choice int16;
+	fmt.Println()
+	fmt.Printf("%-15v", "Enter location index: ")
+	fmt.Scanln(&choice)
+
+	if choice <= int16(length){
+		return choice -1
+	} else {
+		fmt.Println("Invalid index entered!")
+		return getUserIndexChoice(length)
+	}
+}
